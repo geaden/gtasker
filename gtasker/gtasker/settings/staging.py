9fdbf1cac4dc89aa261a9a362bc7fde018e34660
Gennady Denisov <denisovgena@gmail.com>
2013-09-26 21:23:17 +0400

    add staging settings

--- gtasker/gtasker/settings/staging.py
+++ gtasker/gtasker/settings/staging.py
@@ -1,10 +1,11 @@
 from .base import *
 
-from .local import DATABASES
-
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-# DATABASES['default'] =  dj_database_url.config()
+
+DATABASES = {}
+
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -19,7 +20,3 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
